b0f9bdb3f698f9e79016cfcda23d317338dfc5a9
asorsz <asorsz@gmail.com>
2021-03-27 15:18:52 +0000

    Play sound on keydown

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,18 +1,31 @@
+import { useEffect } from 'react';
 import '../css/DrumPad.css';
 import drumPads from './drumpads';
 
 const DrumPad = () => {
 
   const playSound = (e) => { 
-    const audio = document.querySelector(`#${e.target.dataset.key}`)
+    console.log(e)
+
+    const audio = e.type === 'click'
+      ? document.querySelector(`#${e.target.dataset.key}`)
+      : document.querySelector(`#${e.key.toUpperCase()}`)
+        
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     e.target.classList.add('playing');
-  }
+  };
 
-  return (
-    <div id="drum-pad">
+  useEffect(() => {
+    window.addEventListener('keydown', playSound);
+    return () => {
+      window.removeEventListener('keydown', playSound);
+    };
+  }, []);
+
+ return (
+    <div id="drum-pad" onKeyPress={(e) => playSound(e)}>
       {drumPads.map((drumpad) => (
         <div
           id={drumpad.sound}
