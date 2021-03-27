29e7596019a205e20939792e7009a515ffacb094
asorsz <asorsz@gmail.com>
2021-03-27 15:58:19 +0000

    Add state for Display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import '../css/App.css';
 import Display from './Display';
 import DrumPad from './DrumPad';
 
 const App = () => {
+  const [playing, setPlaying] = useState('hello')
+
   return (
     <div id="wrapper" className="container text-center d-flex flex-column justify-content-center align-items-center">
       <div id="drum-machine">   
-        <Display />
-        <DrumPad />
+        <Display playing={playing}/>
+        <DrumPad setPlaying={setPlaying}/>
       </div>
     </div>
   );
